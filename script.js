8f55abe4adf18342753e7959489a24d162c7631f
ksulakshana02 <sulakshanakaveesha@gmail.com>
2024-02-12 03:13:13 +0530

    add js file

--- script.js
+++ script.js
@@ -1,33 +1,41 @@
 let noCount = 0;
-let yesPressed = false;
-let yesButtonSize = noCount * 20 + 16;
 
-const handleNoClick = () =>{
+const handleNoClick = () => {
     noCount++;
+    let yesButtonSize = noCount * 20 + 16;
+    document.getElementById("button2").innerText = (noCount === 0)? "බැහැ" :getNoButtonText();
+    document.getElementById("button1").style.fontSize = `${yesButtonSize}px`;
 }
 
 const getNoButtonText = () => {
     const phrases = [
-        "No",
-        "Are you sure?",
-        "Really sure?",
-        "Think again!",
-        "Last chance!",
-        "Surely not?",
-        "You might regret this!",
-        "Give it another thought!",
-        "Are you absolutely certain?",
-        "This could be a mistake!",
-        "Have a heart!",
-        "Don't be so cold!",
-        "Change of heart?",
-        "Wouldn't you reconsider?",
-        "Is that your final answer?",
-        "You're breaking my heart ;(",
-        "Is that your final answer?",
-        "You're breaking my heart ;(",
-        "Plsss? :( You're breaking my heart"
+        "බැහැ",
+        "ඔයාට විශ්වාසද?",
+        "ඇත්තටමද?",
+        "ආයේ හිතන්නකො",
+        "අන්තිම වතාවට",
+        "හා කියන්නකො",
+        "පස්සෙ පසුතැවෙන්න වෙයි",
+        "හා කියමු නේද?",
+        "ඔයා වැරැද්දක් කරනවා",
+        "බොරු නේද?",
+        "හදවතක් නැද්ද?",
+        "දරුණු වෙන්න එපා!",
+        "මහා නරකයි",
+        "හා කියන්න",
+        "අන්තිම වතාවට",
+        "නපුරු වෙන්න එපා ;(",
+        "මේකද ඔයාගේ අන්තිම තීරණය",
+        "තරහයි මං ;(",
+        "එහෙම කරන්න එපා.. පව්නේ..."
     ];
 
     return phrases[Math.min(noCount, phrases.length - 1)];
 };
+
+const handleYesClick = ()=>{
+    document.querySelector("h1").innerText = "උම්ම්ම්ම්ම්ම්ම්ම්ම්මා😘!!!"
+    document.querySelector("img").src = "https://media.tenor.com/gUiu1zyxfzYAAAAi/bear-kiss-bear-kisses.gif"
+    document.querySelector(".button").style.display = "none"
+
+}
